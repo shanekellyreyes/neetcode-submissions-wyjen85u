27625960feac7724b7151ccc72231c751c22b9b0
Shane Kelly Reyes <shanekellyreyes@gmail.com>
2026-04-03 15:13:06 -0700

    Add: number-of-one-bits - submission-2

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((1 << i & n) != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
